cce43c1ca484efc06a49308b5e3bee7d83a0b8d3
Yev <sharepiece@gmail.com>
2023-01-02 01:12:57 +0200

    refactor login/register logic for better performance

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,7 +1,38 @@
-
+import { authorize } from './majorAuth';
 import { LockClosedIcon } from '@heroicons/react/20/solid'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginForm() {
+  const navigate = useNavigate();
+  const [formValue, setFormValue] = useState({
+    email:'',
+    password:''
+  });
+
+  const handleChange = (e:React.FormEvent<HTMLInputElement>) =>{
+    const value = e.currentTarget.value;
+    const name = e.currentTarget.name;
+    setFormValue({...formValue, [name]: value})
+  }
+
+  const handleSubmit = (e:React.FormEvent)=>{
+    e.preventDefault();
+    if(!formValue.email || !formValue.password){
+      return;
+    }
+    authorize(formValue.email, formValue.password)
+      .then((data)=> {
+        if(data.jwt){
+          setFormValue({email: '', password:''});
+          navigate('/profile', {replace: true});
+        }
+      })
+      .catch(err => console.log(err));
+    }
+
+
+
   return (
     <>
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -16,14 +47,17 @@ export default function LoginForm() {
               Sign in to your account
             </h2>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
+          <form onSubmit={handleSubmit} className="mt-8 space-y-6" >
+            <input 
+            type="hidden" name="remember" defaultValue="true" />
             <div className="-space-y-px rounded-md shadow-sm">
               <div>
                 <label htmlFor="email-address" className="sr-only">
                   Email address
                 </label>
                 <input
+                  onChange={handleChange}
+                  value={formValue.email}
                   id="email-address"
                   name="email"
                   type="email"
@@ -38,6 +72,8 @@ export default function LoginForm() {
                   Password
                 </label>
                 <input
+                  onChange={handleChange}
+                  value={formValue.password}
                   id="password"
                   name="password"
                   type="password"
@@ -71,6 +107,7 @@ export default function LoginForm() {
 
             <div>
               <button
+                onSubmit={handleSubmit}
                 type="submit"
                 className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
--- src/components/majorAuth.tsx
+++ src/components/majorAuth.tsx
@@ -1,15 +1,47 @@
-// export const BASE_URL = 'https://api.nomoreparties.co';
+export const BASE_URL = 'https://api.nomoreparties.co';
 
-// export const register = (username, password, email)=>{
-//     return fetch(`${BASE_URL}/auth/local/register`, {
-//         method: 'POST',
-//         headers:{
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({username, password, email})
-//     })
-//     .then((response)=>{
-//         try
-//     })
-// }
+export const register = (username:string, password:string, email:string)=>{
+    return fetch(`${BASE_URL}/auth/local/register`, {
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username, password, email})
+    })
+    .then((response)=>{
+        try{
+            if (response.status === 200){
+                return response.json();
+            }
+        } catch(e){
+            return (e)
+        }
+    })
+    .then((res)=>{
+        return res;
+    })
+    .catch((err)=> console.log(err));
+}
+
+export const authorize = (identifier:string, password:string)=>{
+    return fetch(`${BASE_URL}/auth/local`, {
+        method: 'POST',
+        headers: {
+            'Accept':'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({identifier, password})
+    })
+    .then((response => response.json()))
+    .then((data)=>{
+        if(data.jwt){
+            localStorage.setItem('jwt', data.jwt)
+            return data;
+        }else{
+            return;
+        }
+    })
+    .catch(err=> console.log(err))
+}
+
